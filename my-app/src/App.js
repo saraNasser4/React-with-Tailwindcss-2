e9dc1e7d2996c2ba1c01dc41fdb6a039465ef4e5
saraNasser4 <saransser6@gmail.com>
2024-10-02 21:12:15 +0300

    finshed all static parts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,20 @@
 import NavBar from './components/NavBar';
 import Landing from './components/LandingCom';
+import Services from './components/Services';
+import Rapidscat from './components/OurMission';
 import Footer from './components/Footer';
+
+import SateliteOne from './assets/satelite1.jpg';
+import SateliteTwo from './assets/satelite2.jpg';
 
 export default function App() {
   return(
     <div>
       <NavBar />
       <Landing />
+      <Services />
+      <Rapidscat reverse={true} img={SateliteOne} header="Rapidseat"/>
+      <Rapidscat reverse={false} img={SateliteTwo} header="Rapidseat"/>
       <Footer />
     </div>
   )
