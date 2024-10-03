a4c3daf306be0b99b810151b4259312544501186
saraNasser4 <saransser6@gmail.com>
2024-10-03 10:08:26 +0300

    adding AOS to the project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import NavBar from './components/NavBar';
 import Landing from './components/LandingCom';
 import Services from './components/Services';
@@ -7,14 +11,23 @@ import Footer from './components/Footer';
 import SateliteOne from './assets/satelite1.jpg';
 import SateliteTwo from './assets/satelite2.jpg';
 
+
 export default function App() {
+  
+  React.useEffect(()=> {
+    AOS.init({
+      duration: 1200,
+      easing: 'ease-in-out',
+    })
+  })
+
   return(
     <div>
       <NavBar />
       <Landing />
       <Services />
-      <Rapidscat reverse={true} img={SateliteOne} header="Rapidseat"/>
-      <Rapidscat reverse={false} img={SateliteTwo} header="Rapidseat"/>
+      <Rapidscat reverse={true} img={SateliteOne} header="Header One"/>
+      <Rapidscat reverse={false} img={SateliteTwo} header="Header two"/>
       <Footer />
     </div>
   )
